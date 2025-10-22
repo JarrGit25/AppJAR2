f21767e696817476fc0603294b22314a11046932
JarrGit25 <temamultiple@gmail.com>
2025-10-22 12:09:56 +0200

    Animaciones 3

--- app/src/main/java/com/curso/AppJAR/animaciones/AnimationActivity.kt
+++ app/src/main/java/com/curso/AppJAR/animaciones/AnimationActivity.kt
@@ -1,19 +1,26 @@
 package com.curso.AppJAR.animaciones
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.curso.AppJAR.Constantes
 import com.curso.AppJAR.R
 
-class AnimationActivity : AppCompatActivity() {
+
+//para las animaciones, la clase debe implementar la interfaz AnimatorListener
+class AnimationActivity : AppCompatActivity(), AnimationListener {
 
     lateinit var imagen1:ImageView
     lateinit var imagen2:ImageView
     lateinit var layoutPadre:LinearLayout
     lateinit var animacion:Animation
+    var pararAnimacion = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +37,35 @@ class AnimationActivity : AppCompatActivity() {
         this.animacion = AnimationUtils.loadAnimation(this, R.anim.animacion)
         this.animacion.reset()
         this.layoutPadre.startAnimation(this.animacion)
+
+        animacion.setAnimationListener(this)
     }
+
+    override fun onAnimationStart(animation: Animation?) {
+        Log.d(Constantes.ETIQUETA_LOG, "onAnimationStart")
+    }
+
+    //cuando finaliza la animacion se actualizan las imagenes y se relanza
+    override fun onAnimationEnd(animation: Animation?) {
+        Log.d(Constantes.ETIQUETA_LOG, "onAnimationEnd")
+        if(!pararAnimacion){
+            this.imagen1.setImageResource(R.mipmap.ic_launcher)
+            this.imagen2.setImageResource(R.mipmap.ic_launcher_round)
+
+            this.layoutPadre.startAnimation(animation)
+        }
+
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) {
+        Log.d(Constantes.ETIQUETA_LOG, "onAnimationRepeat")
+    }
+
+    fun toque(view: View){
+        Log.d(Constantes.ETIQUETA_LOG, "imagen pulsada ... detengo la animación")
+        this.animacion.cancel()
+        this.pararAnimacion = true
+    }
+
+
 }
